40a6876b8a07efb6f7b9390f772ac623c927c02b
Sztorm <Marcinwicher99@gmail.com>
2021-11-30 23:57:55 +0100

    Fixed project reference

--- src/CSharp/Program.cs
+++ src/CSharp/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace AdventOfCode2021
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
